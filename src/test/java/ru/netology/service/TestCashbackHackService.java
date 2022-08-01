173240ffc75830ea2b0fbf968dd6a9b894dfd994
Анна <marinaanna542@gmail.com>
2022-08-01 13:33:20 +0300

    TestNg

---
+++ src/test/java/ru/netology/service/TestCashbackHackService.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestCashbackHackService {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnAddAmountLessBoundary() {
+        int actual = service.remain(900);
+        assertEquals(actual, 100);
+    }
+
+    @Test
+    public void shouldReturnAmount() {
+        int actual = service.remain(0);
+        assertEquals(actual, 1000);
+    }
+
+    @Test
+    public void shouldNotReturnAddAmount() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldReturnAddAmountOverBoundary() {
+        assertEquals(service.remain(1100), 900);
+    }
+}
